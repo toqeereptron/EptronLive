655c3f145b753add3ddf7a2cb42affd8a0504f5e
toqeer2.test <toqeer@eptron.eu>
2016-03-16 14:51:55 +0500

    Products.html page
    all is done except slider pagination issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-// JavaScript Document
+
+$(document).ready(function () {
+    $('.product_slider').bxSlider({
+        mode: 'fade',
+        auto: 'true',
+        nextText: '',
+        prevText: '',
+        pagerSelector: '',
+    });
+});
